42c0ac4f12c04b1dd559663256aa92216e0b6ba0
anggaawijaya <anggaa_wijaya@yahoo.com>
2020-02-12 18:42:16 +0700

    test

---
+++ day1.js
@@ -0,0 +1,19 @@
+
+// Koding itu dibaca dari atas kebawah,  kiri ke kanan
+
+console.log('halo jc12');
+console.log('saya angga');
+console.log(1231234);
+console.log("'kamu siapa'");
+console.log('"siapa aja boleh"');
+var x= 23;
+var y= 24;
+
+var nama= 'angga';
+var usia= 23;
+
+console.log(nama+" usianya " +usia);
+
+
+//sama sekali gak perlu define tipe data?
+//boolean OR / AND
